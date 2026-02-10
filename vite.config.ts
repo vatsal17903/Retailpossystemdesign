a52dd89292b5c69bd7a91b489d580308b4e6a874
vatsal17903 <bhalodiavatsal2003@gmail.com>
2026-02-11 02:16:44 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Retailpossystemdesign/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
